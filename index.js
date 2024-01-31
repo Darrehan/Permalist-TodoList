9255a7f8dacf482a8d190cd78e48dd9a55916f11
Darrehan <darrehanrasool@gmail.com>
2024-01-31 23:36:57 +0530

    Dar Rehan Rasool
    sycomusiclover

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-
 const app = express();
 const port = 4000;
-
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
@@ -13,18 +11,16 @@ const db = new pg.Client({
   port: 5432,
 });
 db.connect();
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
+  // { id: 1, title: "Buy milk" },
+  // { id: 2, title: "Finish homework" },
 ];
 app.get("/", async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM items ORDER BY id ASC");
     items = result.rows;
-
     res.render("index.ejs", {
       listTitle: "Today",
       listItems: items,
@@ -35,6 +31,7 @@ app.get("/", async (req, res) => {
 });
 app.post("/add", async (req, res) => {
   const item = req.body.newItem;
+  // Alternative method Destructuring concept
   // items.push({title: item});
   try {
     await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
